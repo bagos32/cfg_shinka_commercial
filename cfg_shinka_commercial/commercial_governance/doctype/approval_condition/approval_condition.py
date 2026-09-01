d76a2f9ae90452f549fd425ca8814f1240215832
Bagos <erp@ip-172-31-4-211.ap-south-1.compute.internal>
2026-09-01 16:18:17 +0000

    feat: add governance cross-record defaults and consistency

--- cfg_shinka_commercial/commercial_governance/doctype/approval_condition/approval_condition.py
+++ cfg_shinka_commercial/commercial_governance/doctype/approval_condition/approval_condition.py
@@ -1,9 +1,38 @@
-# Copyright (c) 2026, CFG Shinka and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ApprovalCondition(Document):
-	pass
+    def before_validate(self):
+        self.apply_gate_review_defaults()
+
+    def validate(self):
+        self.validate_gate_review_consistency()
+
+    def apply_gate_review_defaults(self):
+        if not self.gate_review:
+            return
+
+        gate_review = frappe.get_doc(
+            "Gate Review",
+            self.gate_review,
+        )
+
+        if not self.commercial_opportunity:
+            self.commercial_opportunity = gate_review.commercial_opportunity
+
+    def validate_gate_review_consistency(self):
+        if not self.gate_review or not self.commercial_opportunity:
+            return
+
+        gate_opportunity = frappe.db.get_value(
+            "Gate Review",
+            self.gate_review,
+            "commercial_opportunity",
+        )
+
+        if gate_opportunity != self.commercial_opportunity:
+            frappe.throw(
+                "Approval Condition must belong to the same "
+                "Commercial Opportunity as the Gate Review."
+            )
--- cfg_shinka_commercial/commercial_governance/doctype/risk_review/risk_review.py
+++ cfg_shinka_commercial/commercial_governance/doctype/risk_review/risk_review.py
@@ -1,9 +1,21 @@
-# Copyright (c) 2026, CFG Shinka and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class RiskReview(Document):
-	pass
+    def before_validate(self):
+        self.apply_opportunity_defaults()
+
+    def apply_opportunity_defaults(self):
+        if not self.commercial_opportunity:
+            return
+
+        opportunity = frappe.get_doc(
+            "Commercial Opportunity",
+            self.commercial_opportunity,
+        )
+
+        if not self.commercial_development_case:
+            self.commercial_development_case = (
+                opportunity.commercial_development_case
+            )
