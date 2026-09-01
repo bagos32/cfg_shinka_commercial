fcb7861362ebf42895c225796cfd08547827d170
Bagos <erp@ip-172-31-4-211.ap-south-1.compute.internal>
2026-09-01 15:11:20 +0000

    feat: add market intelligence permissions and evidence validation

--- cfg_shinka_commercial/market_intelligence/doctype/evidence_record/evidence_record.py
+++ cfg_shinka_commercial/market_intelligence/doctype/evidence_record/evidence_record.py
@@ -1,9 +1,30 @@
-# Copyright (c) 2026, CFG Shinka and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import nowdate
 
 
 class EvidenceRecord(Document):
-	pass
+    def validate(self):
+        self.validate_verification()
+        self.validate_source_document()
+
+    def validate_verification(self):
+        if self.verification_status == "Verified":
+            if not self.verified_by:
+                frappe.throw(
+                    "Verified By is required when Verification Status is Verified."
+                )
+
+            if not self.verified_date:
+                self.verified_date = nowdate()
+
+        elif self.verified_date and not self.verified_by:
+            frappe.throw(
+                "Verified By is required when Verified Date is populated."
+            )
+
+    def validate_source_document(self):
+        if self.source_document and not self.source_doctype:
+            frappe.throw(
+                "Source DocType is required when Source Document is populated."
+            )
