c655b8378b9491e7476eeaba33ed649c2c0c2c03
Bagos <erp@ip-172-31-4-211.ap-south-1.compute.internal>
2026-09-01 11:24:28 +0000

    feat: add commercial development case doctype

---
+++ cfg_shinka_commercial/commercial_core/doctype/commercial_development_case/commercial_development_case.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, CFG Shinka and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class CommercialDevelopmentCase(Document):
+	pass
